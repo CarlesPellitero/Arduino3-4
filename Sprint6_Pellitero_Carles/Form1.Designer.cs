ed30c2132a0f52c911974ac462cc2e13002e7464
Carles Pellitero <104790563+CarlesPellitero@users.noreply.github.com>
2023-03-14 19:40:05 +0100

    Add files via upload

---
+++ Sprint6_Pellitero_Carles/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Sprint6_Pellitero_Carles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnContaEnrera = new System.Windows.Forms.Button();
+            this.lbtemps = new System.Windows.Forms.Label();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnComprovar = new System.Windows.Forms.Button();
+            this.turnback = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnContaEnrera
+            // 
+            this.btnContaEnrera.Location = new System.Drawing.Point(331, 68);
+            this.btnContaEnrera.Name = "btnContaEnrera";
+            this.btnContaEnrera.Size = new System.Drawing.Size(95, 61);
+            this.btnContaEnrera.TabIndex = 0;
+            this.btnContaEnrera.Text = "Cuenta atras";
+            this.btnContaEnrera.UseVisualStyleBackColor = true;
+            this.btnContaEnrera.Visible = false;
+            this.btnContaEnrera.Click += new System.EventHandler(this.btnContaEnrera_Click);
+            // 
+            // lbtemps
+            // 
+            this.lbtemps.AutoSize = true;
+            this.lbtemps.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtemps.Location = new System.Drawing.Point(314, 142);
+            this.lbtemps.Name = "lbtemps";
+            this.lbtemps.Size = new System.Drawing.Size(87, 17);
+            this.lbtemps.TabIndex = 1;
+            this.lbtemps.Text = "cuanta atras";
+            this.lbtemps.Visible = false;
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(22, 87);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(121, 24);
+            this.cbPorts.TabIndex = 2;
+            this.cbPorts.SelectedIndexChanged += new System.EventHandler(this.cbPorts_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Selecciona el port corresponent";
+            // 
+            // btnComprovar
+            // 
+            this.btnComprovar.Location = new System.Drawing.Point(162, 80);
+            this.btnComprovar.Name = "btnComprovar";
+            this.btnComprovar.Size = new System.Drawing.Size(90, 37);
+            this.btnComprovar.TabIndex = 4;
+            this.btnComprovar.Text = "Connectar";
+            this.btnComprovar.UseVisualStyleBackColor = true;
+            this.btnComprovar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // turnback
+            // 
+            this.turnback.Interval = 1000;
+            this.turnback.Tick += new System.EventHandler(this.turnback_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 232);
+            this.Controls.Add(this.btnComprovar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.lbtemps);
+            this.Controls.Add(this.btnContaEnrera);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Keypad&Pantalla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnContaEnrera;
+        private System.Windows.Forms.Label lbtemps;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnComprovar;
+        private System.Windows.Forms.Timer turnback;
+    }
+}
+
---
+++ Sprint6_Pellitero_Carles/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Threading.Timer;
+
+namespace Sprint6_Pellitero_Carles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SerialPort portArduino;
+        bool obert = false, selecionat = false;
+        Thread thread;
+        int backcount = 30;
+        RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        string xifres;
+
+        public class password
+        {
+            public string lletra { get; set; }
+            public string valors { get; set; }
+        }
+
+        List<password> Password = new List<password>();
+
+
+        private int RandomGenerator()
+        {
+            var byteArray = new byte[4];
+            rngCsp.GetBytes(byteArray);
+            int randomInteger = BitConverter.ToInt32(byteArray, 0);
+            return randomInteger;
+
+        }
+
+        private void GeneradorA1D5()
+        {
+            Queue<int> digits = new Queue<int>();
+            ArrayList lletras = new ArrayList() { "A", "E", "I", "O", "U" };
+            ArrayList array = new ArrayList() { 1, 2, 3, 4, 5 }; ;
+            int digit, digitArray;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                
+                int randomInteger = RandomGenerator();
+
+                for (int j = 0; j < 1; j++)
+                {
+                    Random random = new Random(randomInteger);
+                    digit = random.Next(array.Count);
+                    digitArray = (int)array[digit];
+                    digits.Enqueue(digitArray);
+                    array.Remove(digitArray);
+                }
+
+                while (digits.Count > 0)
+                {
+                    xifres += digits.Dequeue().ToString();
+                }
+
+                password codi = new password();
+
+                codi.lletra = lletras[i].ToString();
+                codi.valors = xifres;
+                Password.Add(codi); 
+
+                xifres = "";
+            }
+            rngCsp.Dispose();
+        }
+
+        private void SeleccionarPort()
+        {
+            //Obrirà el port seleccionat i per verificar que la comunicació ha estat reeixida, 
+            string port;
+            try
+            {
+                port = cbPorts.Text;
+                portArduino = new SerialPort();
+                portArduino.BaudRate = 9600;
+                portArduino.PortName = port;
+                portArduino.Open();
+            }
+            catch (ArgumentException)
+            { }
+
+        }
+
+        private void turnback_Tick(object sender, EventArgs e)
+        {
+            backcount--;
+            lbtemps.Text = "Temps en milisegons: " + (backcount * 1000).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                cbPorts.Items.Add(port);
+            }
+        }
+
+        private void cbPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selecionat = true;
+        }
+
+        private void btnContaEnrera_Click(object sender, EventArgs e)
+        {
+            //Generar numeors aleatoris A1 - D5
+            GeneradorA1D5();
+            turnback = new Timer();
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (!obert && selecionat)
+            {
+                SeleccionarPort();
+                if (portArduino != null)
+                {
+                    btnComprovar.Text = "Desconnectar";
+                    obert = true;
+                    portArduino.Write("ACK\n");
+                    string valor = portArduino.ReadLine();
+                    if (valor == "ACK\r")
+                    {
+                        lbtemps.Visible = true;
+                        btnContaEnrera.Visible = true;
+                    }
+                }
+            }
+            else
+            {
+                if (portArduino != null)
+                {
+                    portArduino.Close();
+                    btnComprovar.Text = "Connectar"; 
+                    obert = false;
+                    lbtemps.Visible = false;
+                    btnContaEnrera.Visible = false;
+                    //thread.Abort();
+                }
+
+            }
+        }
+    }
+}
