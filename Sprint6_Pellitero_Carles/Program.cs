efc9fb8e77cb3cc1faed5fa5a3ad01f4db8b8db8
NoNameNotAlive <104912449+NoNameNotAlive@users.noreply.github.com>
2023-03-21 19:20:24 +0100

    QRCreate/Upload

--- Sprint6_Pellitero_Carles/Program.cs
+++ Sprint6_Pellitero_Carles/Program.cs
@@ -16,7 +16,7 @@ namespace Sprint6_Pellitero_Carles
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new QRGenerator());
         }
     }
 }
