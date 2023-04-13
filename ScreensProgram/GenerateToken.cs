950bb17752c65274c343664f8895b483e37b1ac6
NoNameNotAlive <104912449+NoNameNotAlive@users.noreply.github.com>
2023-04-13 20:00:45 +0200

    tokengenerator

--- ScreensProgram/GenerateToken.cs
+++ ScreensProgram/GenerateToken.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScreensProgram
 {
@@ -11,9 +12,15 @@ namespace ScreensProgram
         //Crear un token para el usuario de dos min.
         public static void GeneratedToken()
         {
-            Guid token = Guid.NewGuid();
-
-            
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+            string token = Convert.ToBase64String(time.Concat(key).ToArray());
+            byte[] data = Convert.FromBase64String(token);
+            DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+            if (when < DateTime.UtcNow.AddMinutes(-10))
+            {
+                MessageBox.Show("Ya ha expirato el token");
+            }
 
         }
     }
